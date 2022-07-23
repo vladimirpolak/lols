1ebfe57f231ced44584e0eacce2562d7c07e745f
Vlado <htcvlado@gmail.com>
2022-07-23 19:11:36 +0200

    Added 'model_name' extraction to planetsuzy crawler

--- scrapers/planetsuzy.py
+++ scrapers/planetsuzy.py
@@ -19,6 +19,7 @@ class PlanetSuzyCrawler(CrawlerBase):
     DOMAIN = "planetsuzy.org"
     DESC = "PlanetSuzy Forum Thread"
     CONTENT_TYPE = "THREAD"
+    MODEL_NAME = ""
     SAMPLE_URLS = [
         "http://planetsuzy.org/t967900-daisy-keech.html",
         "http://www.planetsuzy.org/t977950-niece-waidhofer.html",
@@ -28,7 +29,6 @@ class PlanetSuzyCrawler(CrawlerBase):
     def _crawl_link(self, url):
         # Used for creating next page url
         self.thread_path = self.VALID_URL_RE.match(url).group(2)
-
         all_html = ""
 
         while url:
@@ -42,6 +42,8 @@ class PlanetSuzyCrawler(CrawlerBase):
         )
 
         html = response.text
+        if not self.MODEL_NAME:
+            self.MODEL_NAME = self._extract_model_name(html)
         next_page_url = self._get_next_page(html)
 
         return html, next_page_url
@@ -60,4 +62,10 @@ class PlanetSuzyCrawler(CrawlerBase):
             return next_page_url
         return None
 
-
+    def _extract_model_name(self, html) -> Union[str, None]:
+        pattern = re.compile(r"<title>([\d\w\s]+) - Free Porn & Adult Videos Forum</title>")
+        results = pattern.findall(html)
+        if results:
+            album_title = results[0].strip()
+            return album_title
+        return None
