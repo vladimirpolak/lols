c529296ad8e42aca4ef4203b68293a6851787bf0
Vlado <htcvlado@gmail.com>
2022-08-24 17:16:48 +0200

    Working Highload Video Extractor

--- scrapers/highload.py
+++ scrapers/highload.py
@@ -1,13 +1,10 @@
-from ._scraper_base import ExtractorBase, CrawlerBase
-from downloader.types import determine_content_type_, img_extensions, vid_extensions
-from exceptions import ExtractionError, ScraperInitError
-from config import Manager as config
+from ._scraper_base import ExtractorBase
+from downloader.types import determine_content_type_, vid_extensions
+from exceptions import ExtractionError
 from utils import split_filename_ext, decode_base64
-import logging
 import re
-import json
-# import js2py
-# result = js2py.eval_js(script)
+import js2py
+
 
 URL_HIGHLOAD_MASTERJS = "https://highload.to/assets/js/master.js"
 
@@ -27,36 +24,52 @@ class HighloadVideoExtractor(ExtractorBase):
     ]
 
     def _extract_data(self, url):
+        self.origin_url = url
         response = self.request(
-            url=url,
+            url=self.origin_url,
         )
         html = response.text
 
         # extract vars_script
         vars_script = self._extract_vars_script(html)
-
         # prepare vars_script
         prepped_vars_script = self._prepare_script(vars_script)
-
         # get vars_script output with js2py
-        # vars_script_output = js2py.eval_js(prepped_vars_script)
+        vars_script_output = js2py.eval_js(prepped_vars_script)
 
         # Request master.js
         master_script = self._get_masterjs(
-            referer=url
+            referer=self.origin_url
         )
 
         # prepare master_script
         prepped_master_script = self._prepare_script(master_script)
-
         # get master_script output with js2py
-        # master_script_output = js2py.eval_js(prepped_master_script)
-
+        master_script_output = js2py.eval_js(prepped_master_script)
         # extract source_variable, res_1 value and res_2 value
-        # extract source_variable value from variables_script
-        # decode the direct link based off values of source_variable, res_1 and res_2
+        source_var = self._extract_source_var_name(master_script_output)
+        res_1, res_2 = self._extract_res1_res2(master_script_output)
 
-        self._get_masterjs(referer=url)
+        # extract source_variable value from variables_script
+        source_value = self._extract_source_var_value(
+            javascript=vars_script_output,
+            var_name=source_var
+        )
+        # decode the direct link based off values of source_variable, res_1 and res_2
+        source = self._decode_direct_link(
+            source_value, res_1, res_2
+        )
+
+        file_w_ext = self.origin_url.split("/")[-1]
+        filename, extension = split_filename_ext(file_w_ext)
+        content_type = determine_content_type_(extension)
+
+        self.add_item(
+            source=source,
+            filename=filename,
+            extension=extension,
+            content_type=content_type
+        )
 
     def _get_masterjs(self, referer: str):
         """Returns master.js script."""
@@ -68,7 +81,6 @@ class HighloadVideoExtractor(ExtractorBase):
             headers=headers
         )
         return response.text
-
 
     @staticmethod
     def _prepare_script(javascript: str):
@@ -83,13 +95,13 @@ class HighloadVideoExtractor(ExtractorBase):
             r"(?P<main_func>.*})"
             r"(?P<main_params>\(.*)\)"
         )
-        match = pattern.search(javascript)
-        if not match:
+        result = pattern.search(javascript)
+        if not result:
             raise ValueError("Couldn't match regex.")
 
-        first_half = match.group('first_half')
-        main_func = match.group('main_func')
-        main_params = match.group('main_params')
+        first_half = result.group('first_half')
+        main_func = result.group('main_func')
+        main_params = result.group('main_params')
         return f"{first_half};function main {main_func};main{main_params}"
 
     @staticmethod
@@ -99,7 +111,7 @@ class HighloadVideoExtractor(ExtractorBase):
         )
         result = pattern.search(html)
         if not result:
-            raise ExtractionError()
+            raise ExtractionError("Failed to extract 'vars_script'.")
         return result.group('vars_script')
 
     @staticmethod
@@ -107,10 +119,31 @@ class HighloadVideoExtractor(ExtractorBase):
         pattern = re.compile(
             rf'var res = (\w+)\.replace'
         )
-        match = pattern.search(javascript)
-        if not match:
-            raise ExtractionError()
-        return match.group(1)
+        result = pattern.search(javascript)
+        if not result:
+            raise ExtractionError("Failed to extract source variable name from 'master.js'.")
+        return result.group(1)
+
+    @staticmethod
+    def _extract_source_var_value(javascript: str, var_name: str):
+        pattern = re.compile(
+            rf'var {var_name}[\s=]+"(.*?)";'
+        )
+        result = pattern.search(javascript)
+        if not result:
+            raise ExtractionError(f"Failed to extract source value from vars_script, source var: {var_name}")
+        return result.group(1)
+
+    @staticmethod
+    def _extract_res1_res2(javascript: str):
+        pattern = re.compile(
+            rf'var res[\s=]+(\w+)\.replace\("(?P<res_1>.*?)"[\s,"]+\);?\s*'
+            rf'var res2[\s=]+res\.replace\("(?P<res_2>.*?)"[\s,"]+\);'
+        )
+        result = pattern.search(javascript)
+        if not result:
+            raise ExtractionError("Failed to extract res variables from 'master.js'.")
+        return result.group('res_1'), result.group('res_2')
 
     @staticmethod
     def _decode_direct_link(source_var: str, res_1: str, res_2: str):
