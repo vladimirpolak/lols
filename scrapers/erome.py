e6e9d170a28ecbf2fdfe8de85897f81b2af222fe
Vladimir <htcvlado@gmail.com>
2022-08-25 04:33:32 +0200

    Started Erome Album Extractor

--- scrapers/erome.py
+++ scrapers/erome.py
@@ -25,6 +25,8 @@ class EromeAlbumExtractor(ExtractorBase):
         response = self.request(
             url=url,
         )
+        print(response.status_code)
+        print(response.headers)
 
         # self.add_item(
         #     content_type=content_type,
@@ -37,3 +39,22 @@ class EromeAlbumExtractor(ExtractorBase):
     @classmethod
     def extract_from_html(cls, html):
         return [data for data in set(re.findall(cls.VALID_URL_RE, html))]
+
+# Images
+# https://s11.erome.com/981/69MH41fn/UwQFq07e.png?v=1661108453
+# https://s11.erome.com/981/69MH41fn/F75mTh6A.png?v=1661108459
+# https://s11.erome.com/981/69MH41fn/PQWXAKy4.png?v=1661108433
+# https://s18.erome.com/902/srArB7Yt/wS1CDdpO.jpeg?v=1656038699
+# https://s18.erome.com/902/srArB7Yt/ifzS65Qu.jpeg?v=1656038710
+# https://s18.erome.com/902/srArB7Yt/xt8qX5r6.jpeg?v=1656038713
+# https://s18.erome.com/902/srArB7Yt/w5ZZXRem.jpeg?v=1656038724
+# https://s18.erome.com/902/srArB7Yt/cm1H6w5F.jpeg?v=1656038739
+
+
+# Videos
+# https://s11.erome.com/981/69MH41fn/IXs23YUp_720p.mp4
+# https://s18.erome.com/902/srArB7Yt/DTGmT1Qo_720p.mp4
+# https://s18.erome.com/902/srArB7Yt/2CVMSZFc_720p.mp4
+# https://s18.erome.com/902/srArB7Yt/BdusAg2n_720p.mp4
+# https://s18.erome.com/902/srArB7Yt/jTleLhy9_720p.mp4
+# https://s18.erome.com/902/srArB7Yt/GM8ueJ92_720p.mp4
