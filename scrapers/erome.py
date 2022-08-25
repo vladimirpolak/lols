96442db0c221c50e0c3c35cc42ead047e80b3ae0
Vlado <htcvlado@gmail.com>
2022-08-25 18:30:39 +0200

    Finished Erome Extractor

--- scrapers/erome.py
+++ scrapers/erome.py
@@ -1,13 +1,11 @@
-from ._scraper_base import ExtractorBase, CrawlerBase
+from ._scraper_base import ExtractorBase
 from downloader.types import determine_content_type_, img_extensions, vid_extensions
-from exceptions import ExtractionError, ScraperInitError
-from config import Manager as config
 from utils import split_filename_ext
-import logging
 import re
-import json
 
-PATTERN_EROME_ALBUM = r"(?:https://)?(www\.)erome\.com/a/\w+"
+PATTERN_EROME_ALBUM = r"(?:https://)?(?:www\.)?erome\.com/a/\w+"
+PATTERN_EROME_IMAGE = rf"(?:https://)?s\d+\.erome\.com/\d+/\w+/\w+(?:{'|'.join(img_extensions)})\?v=\d+"
+PATTERN_EROME_VIDEO = rf"(?:https://)?s\d+\.erome\.com/\d+/\w+/\w+(?:{'|'.join(vid_extensions)})"
 
 
 class EromeAlbumExtractor(ExtractorBase):
@@ -25,38 +23,48 @@ class EromeAlbumExtractor(ExtractorBase):
         response = self.request(
             url=url,
         )
-        print(response.status_code)
-        print(response.headers)
+        html = response.text
 
-        # self.add_item(
-        #     content_type=content_type,
-        #     filename=filename,
-        #     extension=extension,
-        #     source=source,
-        #     album_title=album_title
-        # )
+        urls = []
+        urls.extend(self.extract_image_urls(html))
+        urls.extend(self.extract_video_urls(html))
+
+        for url in urls:
+            filename, extension, content_type = self.parse_url(url)
+            headers = {
+                'referer': 'https://www.erome.com/'
+            }
+
+            self.add_item(
+                source=url,
+                filename=filename,
+                extension=extension,
+                content_type=content_type,
+                headers=headers
+            )
+
+    def extract_image_urls(self, html: str, pattern: str = PATTERN_EROME_IMAGE) -> list:
+        return self._extract(pattern, html)
+
+    def extract_video_urls(self, html: str, pattern: str = PATTERN_EROME_VIDEO) -> list:
+        return self._extract(pattern, html)
+
+    @staticmethod
+    def _extract(pattern, html):
+        return list(set(re.findall(pattern, html)))
+
+    def parse_url(self, url: str):
+        filename_w_extension = self._extract_filename(url)
+        filename, extension = split_filename_ext(filename_w_extension)
+        content_type = determine_content_type_(extension)
+        return filename, extension, content_type
+
+    @staticmethod
+    def _extract_filename(url: str) -> str:
+        if re.match(PATTERN_EROME_IMAGE, url):
+            return url.split("?v")[0].split("/")[-1]
+        return url.split("/")[-1]
 
     @classmethod
     def extract_from_html(cls, html):
         return [data for data in set(re.findall(cls.VALID_URL_RE, html))]
-
-# Images
-# rf"(?:https://)?s\d+\.erome\.com/\d+/\w+/\w+(?:{'|'.join(img_extensions)})?v=\d+"
-# https://s11.erome.com/981/69MH41fn/UwQFq07e.png?v=1661108453
-# https://s11.erome.com/981/69MH41fn/F75mTh6A.png?v=1661108459
-# https://s11.erome.com/981/69MH41fn/PQWXAKy4.png?v=1661108433
-# https://s18.erome.com/902/srArB7Yt/wS1CDdpO.jpeg?v=1656038699
-# https://s18.erome.com/902/srArB7Yt/ifzS65Qu.jpeg?v=1656038710
-# https://s18.erome.com/902/srArB7Yt/xt8qX5r6.jpeg?v=1656038713
-# https://s18.erome.com/902/srArB7Yt/w5ZZXRem.jpeg?v=1656038724
-# https://s18.erome.com/902/srArB7Yt/cm1H6w5F.jpeg?v=1656038739
-
-
-# Videos
-# rf"(?:https://)?s\d+\.erome\.com/\d+/\w+/\w(?:{'|'.join(vid_extensions)})"
-# https://s11.erome.com/981/69MH41fn/IXs23YUp_720p.mp4
-# https://s18.erome.com/902/srArB7Yt/DTGmT1Qo_720p.mp4
-# https://s18.erome.com/902/srArB7Yt/2CVMSZFc_720p.mp4
-# https://s18.erome.com/902/srArB7Yt/BdusAg2n_720p.mp4
-# https://s18.erome.com/902/srArB7Yt/jTleLhy9_720p.mp4
-# https://s18.erome.com/902/srArB7Yt/GM8ueJ92_720p.mp4
