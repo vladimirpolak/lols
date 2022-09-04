a7b4b1414aed74e93a5bfa3f8d2c8ac3980b1cef
Vlado <htcvlado@gmail.com>
2022-09-04 13:32:04 +0200

    Disabled Xvideos because not functional

--- scrapers/_all.py
+++ scrapers/_all.py
@@ -16,7 +16,7 @@ from .pixhost import PixHostTHExtractor
 from .imagebam import ImageBamExtractor
 # from .forum_thotsbay import (ForumThotsbayCrawler,
 #                              ForumThotsbayImageExtractor)
-from .xvideos import XvideosVideoExtractor
+# from .xvideos import XvideosVideoExtractor
 from .forum_nudostar import (ForumNudostarCrawler,
                              ForumNudostarImageExtractor,
                              ForumNudostarVideoExtractor)
