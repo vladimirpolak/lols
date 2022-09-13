025b1a543161bec02ae17b406e96d5f4db09439f
Vlado <htcvlado@gmail.com>
2022-09-13 04:52:38 +0200

    Implemented TestCase from unittest library

--- tests/url_matching.py
+++ tests/url_matching.py
@@ -1,15 +1,10 @@
+from unittest import TestCase
 from scrapers import get_scraper_classes
 
 
-class UrlMatchingTest:
-    @classmethod
-    def test(cls):
+class UrlMatchingTest(TestCase):
+    def test_patterns(self):
         scraper_classes = get_scraper_classes()
         for scraper in scraper_classes:
             for url in scraper.SAMPLE_URLS:
-                if not scraper.is_suitable(url):
-                    print(f"{scraper.__name__} FAILED TO MATCH: {url}")
-
-
-if __name__ == '__main__':
-    UrlMatchingTest.test()
+                self.assertTrue(scraper.is_suitable(url), f"{scraper.__name__} FAILED TO MATCH: {url}")
