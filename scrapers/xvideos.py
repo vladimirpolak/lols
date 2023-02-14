da6a9b054c589b4ea485ff493ce4ffbf6ed23164
Vlado <htcvlado@gmail.com>
2023-02-15 00:24:23 +0100

    Xvideos removed "extract_from_html" method

--- scrapers/xvideos.py
+++ scrapers/xvideos.py
@@ -55,8 +55,3 @@ class XvideosVideoExtractor(ExtractorBase):
         if not result:
             raise ExtractionError("Failed to extract title from html.")
         return result.group("title")
-
-    @classmethod
-    def _extract_from_html(cls, html):
-        # return [data for data in set(re.findall(cls.VALID_URL_RE, html))]
-        pass
